c2d00b16a71f50226838fc796cd0c6aee1c97d64
Ameer Arsala <ameer.arsala03@gmail.com>
2023-10-01 23:51:15 -0700

    Initial commit

---
+++ Lab_1/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Ameer Arsala
+ *
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World!");
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# CSC 365
+
+My coursework for 365 at California Polytechnic State University - San Luis Obispo
