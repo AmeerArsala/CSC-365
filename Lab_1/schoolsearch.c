2543fd171f3937fd2a75cbba452312ecad04fc45
Ameer Arsala <56413920+AmeerArsala@users.noreply.github.com>
2023-10-02 23:59:22 +0000

    from 225

--- Lab_1/schoolsearch.c
+++ Lab_1/schoolsearch.c
@@ -11,6 +11,32 @@
 
 #include "schoolsearch.h"
 
+const struct Name NO_NAME = {NULL, NULL};
+const Student NULL_STUDENT = {.studentName = NO_NAME, 
+                              .grade = -1,
+                              .classroom = -1
+                              .bus = -1,
+                              .gpa = -1.0,
+                              .teacherName = NO_NAME };
+
+const CommandArg CMD_STUDENT = {'S', "Student"};
+const CommandArg CMD_TEACHER = {'T', "Teacher"};
+const CommandArg CMD_BUS = {'B', "Bus"};
+const CommandArg CMD_GRADE = {'G', "Grade"};
+const CommandArg ARG_HIGH = {'H', "High"};
+const CommandArg ARG_LOW = {'L', "Low"};
+const CommandArg CMD_AVERAGE = {'A', "Average"};
+const CommandArg CMD_INFO = {'I', "Info"};
+const CommandArg CMD_QUIT = {'Q', "Quit"};
+
+Student MakeQuery(Student studentInfo) {
+
+}
+
+struct Name MakeName(struct Name name) {
+    struct Name nameFinal = NO_NAME
+}
+
 char* toString(Student* studentInfo) {
     Student student = *studentInfo;
     
--- Lab_1/schoolsearch.h
+++ Lab_1/schoolsearch.h
@@ -8,12 +8,25 @@
 #ifndef SCHOOLSEARCH_H
 #define SCHOOLSEARCH_H
 
+extern const struct Name NO_NAME;
+extern const Student NULL_STUDENT;
+
+extern const CommandArg CMD_STUDENT;
+extern const CommandArg CMD_TEACHER;
+extern const CommandArg CMD_BUS;
+extern const CommandArg CMD_GRADE;
+extern const CommandArg ARG_HIGH;
+extern const CommandArg ARG_LOW;
+extern const CommandArg CMD_AVERAGE;
+extern const CommandArg CMD_INFO;
+extern const CommandArg CMD_QUIT;
+
 typedef struct {
     struct Name studentName;
     int grade;
     int classroom;
     int bus; /* you make me busss */
-    float gpa;
+    double gpa;
     struct Name teacherName;
 } Student;
 
@@ -22,7 +35,22 @@ struct Name {
     char* firstName;
 };
 
+typedef struct {
+    char shorthand;
+    char* full;
+} CommandArg;
+
+Student MakeQuery(Student studentInfo);
+struct Name MakeName(struct Name name);
+
+/* (Student* studentInfo) => studentInfo pointer REFERENCES the value *studentInfo */
+
 char* toString(Student* studentInfo);
+
+Student fromLine(char* line);
+/* TODO: fromLines */
+
+Student* findStudentsByInfo(Student* studentInfo);
 
 
 
